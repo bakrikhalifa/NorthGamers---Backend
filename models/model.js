d5a0d82409002a0c0eddf5332109badc06b7fcbf
Bakri Khalifa <bakrikhalifa@Bakris-MacBook-Air.local>
2022-12-15 16:27:05 +0000

    added test and arrows to show refactored code"

--- models/model.js
+++ models/model.js
@@ -65,7 +65,7 @@ exports.getReviewsData = (queryObj) => {
 };
 
 exports.getReviewByIDData = (review_id) => {
-  const queryString = `SELECT reviews.*, COUNT (comment_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id`;
+  const queryString = `SELECT reviews.*, COUNT (comment_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id`; ("<<", "refactored code")
 
   return db.query(queryString, [review_id]).then(({ rows: review }) => {
     if (review[0] === undefined) {
